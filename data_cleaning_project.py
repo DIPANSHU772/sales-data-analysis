8e5c3fd8dceff9a2ef86cec7fb05dec8a807a749
DIPANSHU772 <kashyapdipanshu300@gmail.com>
2026-03-22 17:52:00 +0530

    Implement data cleaning for missing values
    Added data cleaning steps to handle missing values in the DataFrame.

--- data_cleaning_project.py
+++ data_cleaning_project.py
@@ -1,1 +1,21 @@
 
+import pandas as pd
+
+data = {
+    "Name":["Amit","Ravi",None,"Neha","Rahul"],
+    "Age":[23,25,None,22,24],
+    "Salary":[50000,None,45000,48000,None]
+}
+
+df = pd.DataFrame(data)
+
+print("Original Data:")
+print(df)
+
+# Data cleaning
+df["Name"].fillna("Unknown", inplace=True)
+df["Age"].fillna(df["Age"].mean(), inplace=True)
+df["Salary"].fillna(df["Salary"].median(), inplace=True)
+
+print("\nCleaned Data:")
+print(df)
